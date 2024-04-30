6f76f6f8d00502ae993cb0a07eee8479db5dd9c1
unknown <maxezay94@gmail.com>
2024-04-30 05:02:18 -0300

    Calculando el total a pagar en el carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 
 export default function Header({cart}) {
     
+    // State Derivado
+    const isEmpty = () => cart.length === 0
+    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price),0)
+
     return (
     <header className="py-5 header">
         <div className="container-xl">
@@ -17,9 +21,10 @@ export default function Header({cart}) {
                         <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
-                            {cart.length === 0 ? (
+                            { isEmpty() ? (
                             <p className="text-center">El carrito esta vacio</p>
                             ) : (
+                                <>
                             <table className="w-100 table">
                                 <thead>
                                     <tr>
@@ -35,9 +40,9 @@ export default function Header({cart}) {
                                     <tr key={guitar.id}>
                                         <td>
                                             <img
-                                                 className="img-fluid" 
-                                                 src={`/img/${guitar.image}.jpg`} 
-                                                 alt="imagen guitarra" 
+                                                className="img-fluid" 
+                                                src={`/img/${guitar.image}.jpg`} 
+                                                alt="imagen guitarra" 
                                             />
                                         </td>
                                         <td>{guitar.name}</td>
@@ -71,9 +76,13 @@ export default function Header({cart}) {
                                 ))}
                                 </tbody>
                             </table>
+
+                            <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                            </>
                             )}
-                            <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
+
                             <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+
                         </div>
                     </div>
                 </nav>
